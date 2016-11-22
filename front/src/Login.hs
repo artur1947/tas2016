19a0e042a5ce414b527bc22c38a072db056cc7ab
Antystenes <morgoth21@gmail.com>
2016-11-22 13:36:00 +0100

    Working registration. 'working' login

--- front/src/Login.hs
+++ front/src/Login.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-
---import Data.Text
---import Text.Hamlet
 module Main where
-
+--import Data.Text
+import Text.Hamlet
 import Text.Blaze.Html.Renderer.String
 import MeinTemplate
 
+kontęt = "<script src=\"js/login.js\"></script><form><div class=\"form-group\"><label for=\"login\" class=\"control-label\">Login:</label><input type=\"text\" class=\"form-control\" id=\"username\"></input><div class=\"form-group\"><label for=\"password\" class=\"control-label\">Password:</label><input class=\"form-control\" id=\"password\" type=\"password\"></input><button type=\"button\" class=\"btn btn-default\" onclick=\"zaloguj()\">Zaloguj</button></form><br><a href=\"register.html\">Zarejestruj</a>"
 main = putStrLn $ renderHtml $
-  webPage "PTTP" "Tu się logujesz :)" mkUrls
+  webPage "PTTP" kontęt mkUrls
--- front/src/MeinTemplate.hs
+++ front/src/MeinTemplate.hs
@@ -6,6 +6,7 @@
 module MeinTemplate  where
 
 import Data.Text
+import Text.Blaze.Html
 import Text.Hamlet
 import Text.Blaze.Html.Renderer.String
 import Language.Haskell.TH
@@ -22,34 +23,93 @@ import Language.Haskell.TH
 --             "Catalog",
 --             "Login"]
 
-data Page = Home | About | Catalog | Login
+data Page = Home | About | Catalog | Login | Register
 
 mkUrls :: Page -> [(Text, Text)] -> Text
 mkUrls Home   _ = "home.html"
 mkUrls About  _ = "about.html"
 mkUrls Catalog _ = "catalog.html"
 mkUrls Login _ = "login.html"
+mkUrls Register _ = "register.html"
 
 webPage :: Text -> Text -> HtmlUrl Page
 webPage title content = [hamlet|
-  <html lang="pl">
+<!DOCTYPE html>
+<html lang="pl">
+
     <head>
-      <meta charset="utf-8">
-      <title>#{toUpper title}
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>#{title}
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/shop-homepage.css" rel="stylesheet">
+
     <body>
-      <h1>#{title}
-      <div class=title>
-      <hr>
-      <div class=linkbar>
-        <ul>
-          <a href=@{Home}> Strona Główna
-          <a href=@{Catalog}> Przeglądaj Katalog
-          <a href=@{About}> Info
-          <a href=@{Login}> Login
-      <hr>
-      <div class=search>
-      <hr>
-      <div class=content>#{content}
-      <hr>
-      <div class=stopka> stopka i kopyrajty
- |]
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation
+                        <span class="icon-bar">
+                        <span class="icon-bar">
+                        <span class="icon-bar">
+                    <a class="navbar-brand" href=@{Home}>Strona Główna
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href=@{Catalog}>Księgarnia<span class="caret">
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#">Fantastyka
+                                <li>
+                                    <a href="#">Kryminał
+                                <li>
+                                    <a href="#">Sensacja
+                                <li>
+                                    <a href="#">Horror
+                                <li>
+                                    <a href="#">Biografie
+                                <li>
+                                    <a href="#">Poradniki
+                        <li id="loglink">
+                            <a href=@{Login}>Zaloguj się
+                        <li>
+                            <a class="dropdown-toggle" data-toggle="dropdown"  href="#">O Nas<span class="caret">
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#">Informacje
+                                <li>
+                                    <a href="#">Autorzy
+                                <li>
+                                    <a href="#">Kontakt
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-3">
+                    <p class="lead" style="font-size: 200%">#{title}
+                    <div class="list-group">
+                        <a href="#" class="list-group-item">PROMOCJE
+                        <a href="#" class="list-group-item">BESTSELLERY
+                        <a href="#" class="list-group-item">NOWOŚCI
+                <div class="col-md-9">
+                    #{preEscapedToMarkup content}
+
+        <div class="container">
+            <hr>
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; Projekt TAS styczeń 2017
+                        <p id="debug">
+
+        <script src="js/jquery.js">
+        <script src="js/mainscript.js">
+        <script src="js/bootstrap.min.js">
+|]
